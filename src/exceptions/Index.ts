c67d7bedf944471bf082c20645324476ea3088d5
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-18 21:57:35 +0700

    bind new exception

--- src/exceptions/Index.ts
+++ src/exceptions/Index.ts
@@ -1,7 +1,9 @@
 
 import { DuplicationWarn as _DuplicationWarn } from './DuplicationWarn'
+import { RouteNotFound as _RouteNotFound } from './RouteNotFound'
 
 
 export namespace Exception {
     export const DuplicationWarn = _DuplicationWarn;
+    export const RouteNotFound = _RouteNotFound;
 }
